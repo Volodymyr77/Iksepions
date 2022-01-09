bb35d318f2e1cca5a36d676f96ed5a40200a9ecb
“Volodymyr <gagarinvov@gmail.com>
2022-01-09 15:48:14 +0200

    caught it tr

---
+++ src/demo2/Print.java
@@ -0,0 +1,37 @@
+package demo2;
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("hello Java");
+        System.out.println(32);
+        System.out.println("sfdsa " + 3);
+
+//        int a = 456544444;
+//        byte b = 126;
+        long l = -343423433;
+        float f = 34.4f;
+        boolean sd = false;
+
+        Object var1 = 356f;
+        double var7 = 356f;
+        float var4 = 356f;
+        char c = 16;
+        int i = 's';
+
+        var n = 1;
+//        var i = Integer.valueOf(2);
+        var str = "java";
+        var x = 22;
+        int y= 129;
+        byte b = 3;
+        int z= x/y;
+        float e = (x/y);
+        byte a = (byte)130;
+        Object ab = (byte) a + b;
+        System.out.println(ab.getClass().getName() + " value: " + ab);
+        System.out.println(z);
+        System.out.println(e);
+        System.out.println(a);
+
+    }
+}
